0eb43d99b681c906bcc16ceecffca2f06380d191
vishruth-r <emailvishruth@gmail.com>
2023-03-20 19:14:34 +0530

    API (part 1)

---
+++ android/services/remote_services.dart
@@ -0,0 +1,20 @@
+import 'dart:html';
+
+import 'package:alexa_shopping_app/';
+import 'package:alexa_shopping_app/models/items.dart';
+import 'package:http/http.dart' as http;
+class RemoteService
+{
+  Future<List<Welcome>?> getItems() async
+  {
+    var client = http.Client();
+
+    var uri = Uri.parse('https://fakestoreapi.com/products');
+    var response = await client.get(uri);
+    if(response.statusCode == 200)
+      {
+        var json = response.body;
+        return itemsFromJson(json);
+      }
+  }
+}
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:alexa_shopping_app/models/items.dart';
 import 'package:flutter/material.dart';
+import 'package:alexa_shopping_app/services/remote_services.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,12 +11,32 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<Welcome>? items;
   var isLoaded = false;
+
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+
+  }
+  getData() async {
+    items = await RemoteService().getItems();
+    if (items != null)
+    {
+      setState
+    }
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home')
+      appBar: AppBar(title: const Text('Home')
       ),
-      body: ListView.builder(itemBuilder: (context, index){
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index){
         return Container(
           child: Text("aisudssfkn"),
         );
